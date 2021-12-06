9f1b864ddfc8a6fb8622e77f80f86e50ee44abf6
epapoutsellis <epapoutsellis@gmail.com>
2021-12-06 19:31:16 +0000

    update repo with nb

--- regularisers.py
+++ regularisers.py
@@ -8,9 +8,9 @@ def tv(u, isotropic=True, direction = "forward", boundaries = "Neumann"):
     DX, DY = G[1], G[0]
   
     if isotropic:
-        return sum(norm(vstack([DX * vec(u), DY * vec(u)]), 2, axis = 0))
+        return sum(norm(vstack([DX @ vec(u), DY @ vec(u)]), 2, axis = 0))
     else:
-        return sum(norm(vstack([DX * vec(u), DY * vec(u)]), 1, axis = 0))
+        return sum(norm(vstack([DX @ vec(u), DY @ vec(u)]), 1, axis = 0))
 
 def tgv(u, w1, w2, alpha0, alpha1, boundaries = "Neumann"):
 
@@ -20,10 +20,10 @@ def tgv(u, w1, w2, alpha0, alpha1, boundaries = "Neumann"):
     G2 = SparseMat_GradientOperator(u.shape, direction = 'backward', order = 1, boundaries = boundaries) 
     divX, divY = G2[1], G2[0]
   
-    return alpha0 * sum(norm(vstack([DX * vec(u) - vec(w1), DY * vec(u) - vec(w2)]), 2, axis = 0)) + \
-           alpha1 * sum(norm(vstack([ divX * vec(w1), divY * vec(w2), \
-                                      0.5 * ( divX * vec(w2) + divY * vec(w1) ), \
-                                      0.5 * ( divX * vec(w2) + divY * vec(w1) ) ]), 2, axis = 0  ) )
+    return alpha0 * sum(norm(vstack([DX @ vec(u) - vec(w1), DY @ vec(u) - vec(w2)]), 2, axis = 0)) + \
+           alpha1 * sum(norm(vstack([ divX @ vec(w1), divY @ vec(w2), \
+                                      0.5 * ( divX @ vec(w2) + divY @ vec(w1) ), \
+                                      0.5 * ( divX @ vec(w2) + divY @ vec(w1) ) ]), 2, axis = 0  ) )
        
 
 def dtv(u, reference, eta, isotropic=True, direction = "forward", boundaries = "Neumann"):
@@ -41,8 +41,8 @@ def dtv(u, reference, eta, isotropic=True, direction = "forward", boundaries = "
     xi_y = tmp_xi_y/denom
     
     # gradient for u
-    u_x = DX*vec(u)
-    u_y = DY*vec(u) 
+    u_x = DX@vec(u)
+    u_y = DY@vec(u) 
     
     inner_prod = multiply(u_x,xi_x) + multiply(u_y,xi_y)
     z1 = u_x - multiply(inner_prod,xi_x)
